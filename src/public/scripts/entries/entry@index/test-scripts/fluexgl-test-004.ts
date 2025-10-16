dfbb813e8ca6811ff2e7cea6e2472d54192b8544
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2025-10-16 16:37:06 +0200

    More testing bullshit

--- src/public/scripts/entries/entry@index/test-scripts/fluexgl-test-004.ts
+++ src/public/scripts/entries/entry@index/test-scripts/fluexgl-test-004.ts
@@ -1,14 +1,30 @@
-import { AudioDevice, EnsureAudioPermission, ResolveAudioOutputDevices, ResolveDefaultAudioOutputDevice } from "@fluexgl/audio";
+import { AudioDevice, EnsureAudioPermission, LoadAudioSource, ResolveDefaultAudioOutputDevice, Channel } from "@fluexgl/audio";
 
 (async function() {
 
+    // Make sure that FluexGL can access audio devices.
     const canAccessAudioDevices = await EnsureAudioPermission();
 
     if(!canAccessAudioDevices) return null;
 
+    // Resolves the default audio output device.
     const audioDevice: AudioDevice | null = await ResolveDefaultAudioOutputDevice();
 
     if(!audioDevice) return;
 
-    console.log(audioDevice);
+    // Get the master channel from the default audio output device.
+    const masterChannel = audioDevice.GetMasterChannel();
+
+    // Create a new empty channel.
+    const channel = new Channel();
+
+    // Label the channel as 'BackgroundMusic'.
+    channel.SetLabel("BackgroundMusic");
+
+    // Attach the 'BackgroundMusic' channel, to the device's master channel.
+    masterChannel.AttachChannel(channel);
+
+    const audioSource = await LoadAudioSource("/assets/data/bruh.mp3");
+
+    console.log(audioSource);
 })();
