2f9b4273eee8f87478594774fde889746cf3f0a1
Rohan Kanhaisingh <tr.kanhaisingh@gmail.com>
2025-10-27 11:08:41 +0100

    Fixed alias linking

--- src/public/scripts/entries/entry@index/test-scripts/fluexgl-test-002.ts
+++ src/public/scripts/entries/entry@index/test-scripts/fluexgl-test-002.ts
@@ -1,11 +1,11 @@
-import { Thread, ThreadOnLoopEvent } from "../../../../../../../FluexGL/lib/src/index";
+// import { Thread, ThreadOnLoopEvent } from "../../../../../../../FluexGL/lib/src/index";
 
-const thread = new Thread();
+// const thread = new Thread();
 
-thread.AddEventListener("update", function(event: ThreadOnLoopEvent) {
-    console.log("Thread update event:", event);
-});
+// thread.AddEventListener("update", function(event: ThreadOnLoopEvent) {
+//     console.log("Thread update event:", event);
+// });
 
-window.addEventListener("load", function() {
-    thread.Start();
-});
+// window.addEventListener("load", function() {
+//     thread.Start();
+// });
